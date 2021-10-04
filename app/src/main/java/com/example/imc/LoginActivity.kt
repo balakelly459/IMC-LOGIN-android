2e01df2ccac3fc0f696c13b330d061f6582d3c5e
balakelly459 <21192911@CFP127ALUNO.LOCAL>
2021-10-04 11:17:34 -0300

    adicionando menu

--- app/src/main/java/com/example/imc/LoginActivity.kt
+++ app/src/main/java/com/example/imc/LoginActivity.kt
@@ -1,11 +1,23 @@
 package com.example.imc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        supportActionBar!!.hide()
+
+        val textViewCriar = findViewById<TextView>(R.id.text_view_criar)
+
+        textViewCriar.setOnClickListener {
+            val abrirNovoUsuarioActivity =
+                    Intent(this, NovoUsuarioActivity::class.java)
+            startActivity(abrirNovoUsuarioActivity)
+        }
     }
 }
