d57347959343b4644b44b22637a76038c3e9ecd7
KELLY MARQUES BALAZSHAZI <21192911@CFP127ALUNO.LOCAL>
2021-10-25 11:26:12 -0300

    cadastro

--- app/src/main/java/com/example/imc/ui/LoginActivity.kt
+++ app/src/main/java/com/example/imc/ui/LoginActivity.kt
@@ -1,11 +1,12 @@
-package com.example.imc
+ package com.example.imc.ui
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.example.imc.R
 
-class LoginActivity : AppCompatActivity() {
+ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
